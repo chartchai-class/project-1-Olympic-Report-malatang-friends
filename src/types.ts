b7a73cc3efeb73c713298221696c1b29aaa7482b
aye-nyeinSan <aye2904@gmail.com>
2024-08-16 16:10:45 +0700

    grabbing country component finished

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface CountryProps {
+  batchcomplete: string;
+  query: Query;
+}
+
+export interface Query {
+  normalized: Normalized[];
+  pages: {
+    [key: string]: Page;
+  };
+}
+
+export interface Normalized {
+  from: string;
+  to: string;
+}
+
+ export interface Page {
+  pageid: number;
+  ns: number;
+  title: string;
+  thumbnail: Thumbnail;
+  extract: string;
+}
+export interface Thumbnail {
+  source: string;
+  width: number;
+  height: number;
+}
--- src/views/Countrydetails.vue
+++ src/views/Countrydetails.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
-   import DetailsHero from '@/components/DetailsHero.vue';
-   import CountryInformation from '@/components/CountryInformation.vue';
-   
+  import DetailsHero from '@/components/DetailsHero.vue';
+  import CountryInformation from '@/components/CountryInformation.vue';
+
+  import { ref, onMounted, watchEffect } from 'vue';
+  import countryService from '@/services/countryService';
+
+  import type { Page } from '@/types';
+
+  const country = ref<Page | null >(null);
+  
+
+  onMounted(() => {
+watchEffect(() => {
+    countryService.getCountryDetails('myanmar').then((response) => {
+        country.value = response;
+      console.log('Country:', country.value);
+    }).catch((error) => {
+      console.error('Error:', error);
+    });
+  });
+  });
 </script>
+
 <template>
- 
- <DetailsHero />
- <div class="columns-2">
-<CountryInformation/>
-
- </div>
-
- 
-    
-
+  <DetailsHero />
+  <div class="columns-2">
+    <CountryInformation :country="country" />
+  
+  </div>
 </template>
