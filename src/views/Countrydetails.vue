0761b0621ad45a9d6221e08c8a4432507b4fb434
aye-nyeinSan <aye2904@gmail.com>
2024-08-16 16:40:46 +0700

    fix Bug

--- src/views/Countrydetails.vue
+++ src/views/Countrydetails.vue
@@ -1,44 +1,41 @@
 <script setup lang="ts">
-
-
+  import DetailsHero from '@/components/DetailsHero.vue';
+  import CountryInformation from '@/components/CountryInformation.vue';
+  import CountryMedalTable from '@/components/CountryMedalTable.vue';
+  import Comments from '@/components/Comments.vue';
   import { ref, onMounted, watchEffect } from 'vue';
-  import countryService from '@/services/countryService';
+  import countryService from '@/services/CountryService';
 
   import type { Page } from '@/types';
 
-  const country = ref<Page | null >(null);
-  
-
-   import DetailsHero from '@/components/DetailsHero.vue';
-   import CountryInformation from '@/components/CountryInformation.vue';
-   import CountryMedalTable from '@/components/CountryMedalTable.vue';
-   import Comments from '@/components/Comments.vue';
-
+  const country = ref<Page | null>(null);
 
   onMounted(() => {
-watchEffect(() => {
-    countryService.getCountryDetails('myanmar').then((response) => {
-        country.value = response;
-      console.log('Country:', country.value);
-    }).catch((error) => {
-      console.error('Error:', error);
+    watchEffect(() => {
+      countryService
+        .getCountryDetails('myanmar')
+        .then((response) => {
+          country.value = response;
+          console.log('Country:', country.value);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
     });
-  });
   });
 </script>
 <template>
- 
- <DetailsHero />
- <div class="md:container mx-auto my-4">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-         <div>
-            <CountryInformation :country="country"/> 
-         </div>
-
-         <div>
-            <CountryMedalTable/>
-         </div>
-        
+  <DetailsHero />
+  <div class="md:container mx-auto my-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div>
+        <CountryInformation :country="country" />
       </div>
- </div>
-<Comments/>
+
+      <div>
+        <CountryMedalTable />
+      </div>
+    </div>
+  </div>
+  <Comments />
+</template>
