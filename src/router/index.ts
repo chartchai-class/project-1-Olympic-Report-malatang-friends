2e863a8b89275652a8bd091c0a78e8050a744e9a
aye-nyeinSan <aye2904@gmail.com>
2024-08-17 13:12:12 +0700

    added progressbar,errorPage  and change white olympic logo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Countrydetails from '@/views/Countrydetails.vue';
+import NotFound from '@/views/NotFound.vue';
+import NetworkError from '@/views/NetworkError.vue';
+import nProgress from 'nprogress';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +13,33 @@ const router = createRouter({
       name: 'CountryDetails',
       component: Countrydetails,
     },
+    {
+      path: '/404/notFound',
+      name: '404-notfound',
+      component: NotFound,
+      props: true,
+  
+    },
+    {
+      path: '/networkError',
+      name: 'NetworkError',
+      component: NetworkError,
+      props: true,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFound,
+      props: true,
+    },
   ],
 });
+
+router.beforeEach(() => {
+  nProgress.start();
+});
+router.afterEach(() => {
+  nProgress.done();
+});
+
 export default router;
