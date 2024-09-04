31e8ba504f2c793d0c30c2deabe2bc7bc7898e18
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-09-04 19:35:53 +0700

    adding pagination

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -15,6 +15,12 @@ import bronze  from "@/assets/bronze1.png";
   const router = useRouter()
 
   const ranks=ref<MedalRank[] | null>(null)
+  const totalRanks=ref(0)
+
+  const hasNextPage=computed(()=>{
+    const totalPages=Math.ceil(totalRanks.value/10)
+    return page.value<totalPages
+  })
 
   const props = defineProps({
     page:{
@@ -24,7 +30,7 @@ import bronze  from "@/assets/bronze1.png";
   })
   const page = computed(() => props.page)
 
-  const totalRanks=ref(0)
+
 
   onMounted(()=>{
     watchEffect(()=>{
@@ -34,6 +40,7 @@ import bronze  from "@/assets/bronze1.png";
         const startIndex = (page.value - 1) * 10;
         const endIndex = startIndex + 10;
         ranks.value=allData.slice(startIndex,endIndex);
+        totalRanks.value=allData.length;
        
       })
       .catch(()=>{
@@ -44,7 +51,7 @@ import bronze  from "@/assets/bronze1.png";
 </script>
 
 <template>
-<div class="bg-scroll bg-no-repeat bg-cover bg-center text-center" :style="{ backgroundImage: `url(${ background })` } ">
+<div class="bg-scroll bg-no-repeat bg-cover bg-center text-center"  :style="{ backgroundImage: `linear-gradient(0deg, rgba(0, 149, 233, 0.7) 8%, rgba(26, 58, 99, 0.7) 42%), url(${ background })` }">
     <!--new element-->
     <div class="justify-center flex">
     <img
@@ -60,8 +67,10 @@ import bronze  from "@/assets/bronze1.png";
       <h1 class="font-poppins font-semibold text-2xl text-white mb-8">
         2024 Paris Olympic Medal Ranking
       </h1>
-      <table class="table-auto w-full rounded border-separate border-spacing-y-3 px-20">
-        <thead class=" sticky top-0  bg-primaryBlue text-left bg-gray-900 tracking-wider text-white  ">
+
+     <div class="overflow-x-auto">
+        <table class="table-auto min-w-full rounded border-separate border-spacing-y-3">
+        <thead class="sticky top-0  bg-primaryBlue text-left bg-gray-900 tracking-wider text-white  ">
           <tr>
             <th class="p-3 text-center">Rank</th>
             <th class="p-2 text-white text-opacity-0 text-center ">Flag</th>
@@ -81,31 +90,27 @@ import bronze  from "@/assets/bronze1.png";
         <tbody>
           
           <medalTable v-for="ranking in ranks" :key="ranking.rank" :ranking="ranking" />
-
-          <div class="flex justify-between items-center">
-            <RouterLink
-                class=" text-[#ffffff] text-left"
-                id="page-prev"
-                :to="{ }"
-                rel="prev"
-                
-            >
-             Prev Page</RouterLink
-            >
-
-            <RouterLink
-                class=" text-[#ffffff] text-right"
-                id="page-next"
-                :to="{ }"
-                rel="next"
-                
-            >
-                Next Page
-            </RouterLink>
-        </div>
-        
         </tbody>
     </table>
+     </div>
+
+    <div class="flex pb-28 pt-8 pagination">
+      <RouterLink class="flex items-center justify-center px-4 h-10 text-base font-medium text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+      :to="{name: 'home-view',query:{page:page-1}}"
+      rel="prev"
+      v-if="page!=1"
+      >Prev
+      </RouterLink>
+
+      <!-- v-if="hasNextPage" -->
+      <RouterLink 
+      class="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+      :to="{name:'home-view',query:{page:page+1}}"
+      rel="next"
+      v-if="hasNextPage"
+      >Next
+      </RouterLink>
+    </div>
     </div>
   </div>
 </template>
