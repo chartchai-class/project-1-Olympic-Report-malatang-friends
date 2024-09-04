4ec48091130ffd426eb61f6349e87db2843a2c80
Ruth <hyangdan0715@gmail.com>
2024-09-04 19:43:51 +0700

    delete HomeView and

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -7,38 +7,35 @@
   import inputBar from '@/components/inputBar.vue'
   import background from '@/assets/background.png';
   import logo from '@/assets/OlympicLogoWhite.png';
+  import  gold  from "@/assets/gold1.png";
+  import silver  from "@/assets/silver1.png";
+  import bronze  from "@/assets/bronze1.png";
 
   const router = useRouter()
 
   const ranks=ref<MedalRank[] | null>(null)
-
   const totalRanks=ref(0)
-  const pageSize = ref(10)
-  const currentPage = ref(1)
+
+  const hasNextPage=computed(()=>{
+    const totalPages=Math.ceil(totalRanks.value/10)
+    return page.value<totalPages
+  })
 
   const props = defineProps({
     page:{
       type: Number,
       required: true
-    },
-    pageSize:{
-      type:Number,
-      required:true
     }
   })
-  const page = computed(() => props.page);
+  const page = computed(() => props.page)
 
-  const hasNextPage=computed(()=>{
-    const totalPages = Math.ceil(totalRanks.value/pageSize.value)
-    return currentPage.value < totalPages
-  })
+
 
   onMounted(()=>{
     watchEffect(()=>{
       OlympicService.getRanks(10, page.value)
       .then((response)=>{
-        ranks.value=response.data.data
-        totalRanks.value = response.headers['x-total-count']
+        const allData=response.data.data;
       })
       .catch(()=>{
         router.push({name: 'NetworkError'})
@@ -48,7 +45,7 @@
 </script>
 
 <template>
-<div class="bg-scroll bg-no-repeat bg-cover bg-center text-center" :style="{ backgroundImage: `url(${ background })` } ">
+<div class="bg-scroll bg-no-repeat bg-cover bg-center text-center"  :style="{ backgroundImage: `linear-gradient(0deg, rgba(0, 149, 233, 0.7) 8%, rgba(26, 58, 99, 0.7) 42%), url(${ background })` }">
     <!--new element-->
     <div class="justify-center flex">
     <img
@@ -61,36 +58,48 @@
       <inputBar />
     </div>
     <div class="flex flex-col items-center">
-      <h1 class="font-poppins font-semibold text-2xl text-sky-900 mb-8">
+      <h1 class="font-poppins font-semibold text-2xl text-white mb-8">
         2024 Paris Olympic Medal Ranking
       </h1>
-      <table class="table-auto w-full rounded border-separate border-spacing-y-3 px-20">
-        <thead class="text-left bg-gray-900 tracking-wider">
+
+      <div class="overflow-x-auto">
+        <table class="table-auto min-w-full rounded border-separate border-spacing-y-3">
+        <thead class="sticky top-0  bg-primaryBlue text-left bg-gray-900 tracking-wider text-white  ">
           <tr>
             <th class="p-3 text-center">Rank</th>
             <th class="p-2 text-white text-opacity-0 text-center ">Flag</th>
             <th class="p-3 text-center">Country</th>
-            <th class="p-3 text-center">Gold</th>
-            <th class="p-3 text-center">Silver</th>
-            <th class="p-3 text-center">Bronze</th>
+            <th class="p-3 text-center">Gold
+              <img :src="gold" alt="gold-medal" class="inline-block brightness-100 w-5 h-5 mb-2"/>
+            </th>
+            <th class="p-3 text-center">Silver
+              <img :src="silver" alt="silver-medal" class="inline-block brightness-100 w-5 h-5 mb-2"/>
+            </th>
+            <th class="p-3 text-center">Bronze
+              <img :src="bronze" alt="bronze-medal" class="inline-block brightness-100 w-5 h-5 mb-2"/>
+            </th>
             <th class="p-3 text-center">Total</th>
           </tr>
         </thead>
         <tbody>
-          <medalTable v-for="ranking in ranks" :key="ranking.rank" :ranking="ranking"/>
+          
+          <medalTable v-for="ranking in ranks" :key="ranking.rank" :ranking="ranking" />
         </tbody>
-      </table>
-      <div class="flex pb-28 pt-8 pagination">
+    </table>
+  </div>
+
+    <div class="flex pb-28 pt-8 pagination">
       <RouterLink class="flex items-center justify-center px-4 h-10 text-base font-medium text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-      :to="{name:'home', query: { page: page - 1, pageSize: pageSize } }"
+      :to="{name: 'home-view',query:{page:page-1}}"
       rel="prev"
-      v-if="page != 1"
+      v-if="page!=1"
       >Prev
       </RouterLink>
-    
+
       <!-- v-if="hasNextPage" -->
-      <RouterLink class="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-      :to="{name:'home', query: { page: page + 1, pageSize: pageSize } }"
+      <RouterLink 
+      class="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+      :to="{name:'home-view',query:{page:page+1}}"
       rel="next"
       v-if="hasNextPage"
       >Next
